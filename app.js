0a83aee96b1fae2b65c31c868c23f7e1fd456407
Mohamed Rizk <mailme.rizk@gmail.com>
2017-06-06 14:06:46 +0200

    changed port

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ models.setDirectory(__dirname+'/models').bind(
       clientOptions: {
         //contactPoints: ['146.185.179.181'],
         contactPoints: ['localhost'],
-        protocolOptions: { port: 9042 },
+        protocolOptions: { port: 9160 },
         keyspace: 'taqcet',
         queryOptions: {consistency: models.consistencies.one}
       },
